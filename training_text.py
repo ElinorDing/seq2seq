beb52e0d8a10354fa46372187a975efeeba0409b
ElinorDing <47607489+ElinorDing@users.noreply.github.com>
2022-07-28 01:59:13 +0100

    ready to train

--- training_text.py
+++ training_text.py
@@ -24,10 +24,34 @@ def preprocessing_function(examples):
 
 
 # gathering and clean dataset
-path = "/Users/dyt/workspace/seqToseq/training_data.csv"
-df = pd.read_csv(path)
-df = df.rename(columns = {"Sentences":"target_text","Corresponding_words":"source_text"})
-df = df[['source_text', 'target_text']]
+
+def merge_dataset(train, val, test):
+    df_train = pd.read_csv(train)
+    df_train = df_train.rename(columns = {"Sentences":"target_text","Corresponding_words":"source_text"})
+    df_train = df_train[['source_text', 'target_text']]
+    df_val = pd.read_csv(val)
+    df_val = df_val.rename(columns = {"Sentences":"target_text","Corresponding_words":"source_text"})
+    df_val = df_val[['source_text', 'target_text']]
+    df_test = pd.read_csv(test)
+    df_test = df_test.rename(columns = {"Sentences":"target_text","Corresponding_words":"source_text"})
+    df_test = df_test[['source_text', 'target_text']]
+
+    train_dataset = datasets.Dataset.from_dict(df_train)
+    val_dataset = datasets.Dataset.from_dict(df_val)
+    test_dataset = datasets.Dataset.from_dict(df_test)
+    my_dataset_dict = datasets.DatasetDict({"train":train_dataset,"val":val_dataset,"test":test_dataset})
+    print(my_dataset_dict)
+    return my_dataset_dict
+
+train = r"/Users/dyt/workspace/seqToseq/clean_data/train/training_clean.csv"
+val = r"/Users/dyt/workspace/seqToseq/clean_data/val/val_clean.csv"
+test = r"/Users/dyt/workspace/seqToseq/clean_data/test/test_clean.csv"
+ready_dataset = merge_dataset(train,val,test)
+
+# path = "/Users/dyt/workspace/seqToseq/training_data.csv"
+# df = pd.read_csv(path)
+# df = df.rename(columns = {"Sentences":"target_text","Corresponding_words":"source_text"})
+# df = df[['source_text', 'target_text']]
 # df['source_text'] = "words to sentence: " + df['source_text']
 # print(df.head(20))
 
@@ -36,9 +60,10 @@ df = df[['source_text', 'target_text']]
 MODEL_NAME = "t5-small"
 tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME)
 
-inputs = preprocessing_function(df)
+tokenized_datasets = ready_dataset.map(preprocess_function, batched=True)
+# inputs = preprocessing_function(df)
 
-labels = inputs["labels"]
+labels = tokenized_datasets["labels"]
 labels = torch.tensor(labels)
 labels[labels == tokenizer.pad_token_id] = -100
 
@@ -49,11 +74,7 @@ model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME)
 
 batch_size = 16
 output = model(
-    input_ids = inputs["input_ids"],
-    attention_mask = inputs["attention_mask"],
+    input_ids = tokenized_datasets["input_ids"],
+    attention_mask = tokenized_datasets["attention_mask"],
     labels = labels
 )
-
-# generating training/validation/test data
-train_df, test_df = train_test_split(df, test_size=0.2)
-print(train_df.shape, test_df.shape)
