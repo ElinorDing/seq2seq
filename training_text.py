e50d90b0b121f42ed9d2a55c206026f7a0cc8540
ElinorDing <47607489+ElinorDing@users.noreply.github.com>
2022-07-30 11:46:43 +0100

    train model3.0

--- training_text.py
+++ training_text.py
@@ -10,12 +10,12 @@ max_target_length = 128
 
 def preprocess_function(examples):
     inputs = ["words to sentence: " + doc for doc in examples["source_text"]]
-    model_inputs = tokenizer(inputs, max_length=max_source_length,truncation=True,return_tensors='pt')
+    model_inputs = tokenizer(inputs,padding=True, max_length=max_source_length,truncation=True,return_tensors='pt')
 
     # Setup the tokenizer for targets
     with tokenizer.as_target_tokenizer():
         target_inputs = [doc for doc in examples["target_text"]]
-        labels = tokenizer(target_inputs,max_length=max_target_length, truncation=True,return_tensors='pt')
+        labels = tokenizer(target_inputs,padding=True,max_length=max_target_length, truncation=True,return_tensors='pt')
 
     model_inputs["labels"] = labels["input_ids"]
     return model_inputs
