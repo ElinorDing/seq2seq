64c87c0f482b2379888013c1ca11b41ea9a9ef62
ElinorDing <47607489+ElinorDing@users.noreply.github.com>
2022-07-30 15:49:17 +0100

    train model5.0

--- training_text.py
+++ training_text.py
@@ -50,7 +50,7 @@ labels[labels == tokenizer.pad_token_id] = -100
 
 model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME)
 
-batch_size = 4
+batch_size = 32
 output = model(
     input_ids = tokenized_datasets["input_ids"],
     attention_mask = tokenized_datasets["attention_mask"],
