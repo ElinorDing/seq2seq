73ccc46083814526358539bd6a4f55b807d6177d
ElinorDing <47607489+ElinorDing@users.noreply.github.com>
2022-08-02 13:49:33 +0100

    memory allocated3.0

--- training_text.py
+++ training_text.py
@@ -50,7 +50,7 @@ labels[labels == tokenizer.pad_token_id] = -100
 
 model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME)
 print("这里")
-print(torch.cuda.memory_allocated())
+print(torch.cuda.memory_allocated(0))
 print("还是这里")
 torch.cuda.max_memory_allocated()
 batch_size = 1
