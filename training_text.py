f23e043873377e00aa5f3601b7b0101557a21788
ElinorDing <47607489+ElinorDing@users.noreply.github.com>
2022-08-18 16:24:10 +0100

    match csv column

--- training_text.py
+++ training_text.py
@@ -354,7 +354,7 @@ def main():
             data_files["train"] = args.train_file[1:]
         if args.validation_file is not None:
             data_files["validation"] = args.validation_file[1:]
-        extension = args.train_file.split(".")[-1]
+        extension = args.train_file[1:].split(".")[-1]
         raw_datasets = load_dataset(extension, data_files=data_files)
     # See more about loading any type of standard or custom dataset (from files, python dict, pandas DataFrame, etc) at
     # https://huggingface.co/docs/datasets/loading_datasets.html.
