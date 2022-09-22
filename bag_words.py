798c8aa8a74fd22b591ec43ba2516d88ba3cfd34
ElinorDing <47607489+ElinorDing@users.noreply.github.com>
2022-09-22 14:25:30 +0100

    location change

--- bag_words.py
+++ bag_words.py
@@ -8,11 +8,12 @@ def extract_data(path):
     # lines = []
     with open(path) as file_in:
         for line in file_in:
+            print(line)
             preprocessed = re.split(r'\s{3,}',line.replace('\t', '   ').replace('\n', ''))
             # lines.append(preprocessed)
             sentence_list.append(preprocessed[5])
             sentence_list.append(preprocessed[6])
-    print(len(sentence_list))
+    # print(len(sentence_list))
     return sentence_list
 
 def give_bags_words(sentences):
@@ -25,11 +26,11 @@ def give_bags_words(sentences):
         corresponding_words.append([token.text for token in doc])
         count += 1
         print(count)
-    print("sentences length is ",len(sentences))
-    print("words length is",len(corresponding_words))
+    # print("sentences length is ",len(sentences))
+    # print("words length is",len(corresponding_words))
     data={'Sentences':sentences,'Corresponding_words':corresponding_words}
     df = pd.DataFrame(data)
-    df.to_csv('training_data.csv')
+    # df.to_csv('training_data.csv')
     # print(len(sen_with_words))
     return df
 
